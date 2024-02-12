2435c11b7943bb744b12e2e21b5904e604393571
SufiHarris <harrissofi02@gmail.com>
2024-02-12 10:39:31 +0530

    Initial Commit

---
+++ GreetingsApp/GreetingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsAppApp.swift
+//  GreetingsApp
+//
+//  Created by Mohammad Haris Sofi on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
